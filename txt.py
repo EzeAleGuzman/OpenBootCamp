183ebf24f4f9fb4d554cd7e782027a1ea789a2b1
Guzman ezequiel alejandro <124918739+Sarabe89@users.noreply.github.com>
2023-05-04 22:42:13 -0300

    Add files via upload

---
+++ txt.py
@@ -0,0 +1,12 @@
+f = open ("archivos\\archivos.txt", "x")
+f.close()
+
+f= open ("archivos\\archivos.txt", "w")
+f.write("Practica de escritura\n")
+f.write("no es muy complicado\n")
+f.write('solo es cuestion de practica...')
+f.close()
+
+
+
+
